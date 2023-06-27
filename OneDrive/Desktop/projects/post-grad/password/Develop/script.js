06ddec553b3a03baf7a50d9605f0e3d6a727f4ec
Oscar Hernandez <oscarhernandezmex@gmail.com>
2023-06-26 19:30:53 -0700

    character code added

--- OneDrive/Desktop/projects/post-grad/password/Develop/script.js
+++ OneDrive/Desktop/projects/post-grad/password/Develop/script.js
@@ -8,3 +8,13 @@ const numbersDOM = document.getElementById("numbers");
 const symbolDOM = document.getElementById("symbol");
 const generatebtn = document.getElementById("generate");
 const form = document.getElementById("passwordGeneratorForm");
+
+// Generating Character Codes For The Application
+
+const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CODES = arrayFromLowToHight(97, 122);
+const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh (58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHih(123, 126));
